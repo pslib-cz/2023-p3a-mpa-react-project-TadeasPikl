d1fd4218fda668e4ce8fbcb5d3bf5f12612f8c41
TadeasPikl <tadeas+1@piklovi.cz>
2024-05-01 17:02:55 +0200

    DEPLOY FINALY WORKS

--- LostCities/vite.config.ts
+++ LostCities/vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://pslib-cz.github.io/2023-p3a-mpa-react-project-TadeasPikl',
   build:
   {
     outDir: 'build',
